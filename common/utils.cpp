34aca3d695674b43139d44b9a366faf72bc21b52
597751447 <597751447@qq.com>
2021-12-10 18:27:44 +0800

    feat1:封装了pcb和一些操作进程时候需要用的功能函数

--- common/utils.cpp
+++ common/utils.cpp
@@ -4,6 +4,7 @@
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 #define TRUE 1
 #define FALSE 0
 #define OK 1
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "common/utils.h"
 int main() {
+    LinkList linkList;
+    Create(linkList);
+    Show(linkList);
     return 0;
 }
